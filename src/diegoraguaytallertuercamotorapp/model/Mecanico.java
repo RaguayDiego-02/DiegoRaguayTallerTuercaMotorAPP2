b274e452c640aed0b68247388fa6a9200249d69c
Diego Raguay <draguay-2026067@kinal.edu.gt>
2026-05-27 21:46:20 -0600

    002 Se agrego Mecanico.java al model

--- src/diegoraguaytallertuercamotorapp/model/Mecanico.java
+++ src/diegoraguaytallertuercamotorapp/model/Mecanico.java
@@ -8,6 +8,6 @@ package diegoraguaytallertuercamotorapp.model;
  *
  * @author USUARIO
  */
-public class NewClass {
+public class Mecanico {
     
 }
--- src/diegoraguaytallertuercamotorapp/model/conexion/Conexion.java
+++ src/diegoraguaytallertuercamotorapp/model/conexion/Conexion.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package diegoraguaytallertuercamotorapp.model.conexion;
+import java.sql.Connection; 
+import java.sql.DriverManager; 
+import java.sql.SQLException; 
 
-/**
- *
- * @author USUARIO
- */
 public class Conexion {
+    private static Conexion instancia; 
     
+    private static final String URL = "jdbc:mysql://localhost:3306/script_db_DDL_Diego_Raguay_in4cm?serverTimezone=UTC";
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "DiegoRaguay22"; 
+    
+    private Conexion() {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+        }catch(ClassNotFoundException e) {
+            System.err.println("Error Driver: " + e.getMessage());
+        }
+    }
+    public static synchronized Conexion getInstancia() {
+        if (instancia == null) {
+            instancia = new Conexion();
+        }
+        return instancia; 
+    }
+    public Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);    
+    }
 }

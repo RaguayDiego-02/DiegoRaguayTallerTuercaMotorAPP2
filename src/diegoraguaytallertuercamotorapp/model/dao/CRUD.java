bec2d95578a7bc5843527fb7abac35525f337fd2
Diego Raguay <draguay-2026067@kinal.edu.gt>
2026-05-28 16:56:37 -0600

    003 corrige CRUD.java de java class a java interface

--- src/diegoraguaytallertuercamotorapp/model/dao/CRUD.java
+++ src/diegoraguaytallertuercamotorapp/model/dao/CRUD.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package diegoraguaytallertuercamotorapp.model.dao;
+import java.util.List;
 
-/**
- *
- * @author USUARIO
- */
-public class CRUD {
-    
+public interface CRUD<T, ID> {
+    List<T> listar();
 }

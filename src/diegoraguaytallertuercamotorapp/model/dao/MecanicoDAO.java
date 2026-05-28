c45d62204ffc16c2be3d2154f705d914995b9e33
Diego Raguay <draguay-2026067@kinal.edu.gt>
2026-05-28 17:44:10 -0600

    005 Se agrego MecanicoDAOImpl.java

--- src/diegoraguaytallertuercamotorapp/model/dao/MecanicoDAO.java
+++ src/diegoraguaytallertuercamotorapp/model/dao/MecanicoDAO.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package diegoraguaytallertuercamotorapp.model.dao;
+import diegoraguaytallertuercamotorapp.model.Mecanico; 
 
-/**
- *
- * @author USUARIO
- */
-public interface MecanicoDAO {
-    
+public interface MecanicoDAO extends CRUD<Mecanico, Integer> {
 }

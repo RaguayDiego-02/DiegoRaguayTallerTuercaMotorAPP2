6cdc3e2d8ee92b8122bd616691d847e9dd004fa4
Diego Raguay <draguay-2026067@kinal.edu.gt>
2026-05-28 21:49:37 -0600

    008 se agrego MenuPrincipal.java y Principal.java

---
+++ src/diegoraguaytallertuercamotorapp/view/MenuPrincipal.java
@@ -0,0 +1,31 @@
+package diegoraguaytallertuercamotorapp.view;
+import java.util.Scanner; 
+import diegoraguaytallertuercamotorapp.controller.MecanicoController; 
+
+public class MenuPrincipal {
+    private final Scanner scanner = new Scanner (System.in); 
+    
+    public void arrancarSistema() {
+        int opcion;
+        do {
+            System.out.println("\n|______________________________________|");
+            System.out.println("  |Sistema central - TallerTuercaMotorApp|");
+            System.out.println("  |______________________________________|");
+            System.out.println("  | 1). Entrar al modulo de Mecanicos    |");
+            System.out.println("  | 2). Salir del sistema                |");
+            System.out.println("  |______________________________________|");
+            System.out.print("Seleccione una opcion");
+            opcion = scanner.nextInt();
+            
+            switch (opcion) {
+                case 1 ->  {
+                    MecanicoView vistaMec = new MecanicoView();
+                    MecanicoController controlMec = new MecanicoController(vistaMec);
+                    controlMec.iniciarModulo();
+                }
+                case 2 -> System.out.println("\nCerrando sistema...");
+                default -> System.out.println("\nOpcion no valida");     
+            }
+        }while (opcion != 2);
+    }
+}
